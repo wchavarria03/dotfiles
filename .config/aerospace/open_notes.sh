16fe493f58884d7b3e9d927f5e693498c287a3b6
Walter Chavarria <wchavarria03@gmail.com>
2025-03-24 18:12:06 -0600

    impro: aerospace config updated

--- .config/aerospace/open_notes.sh
+++ .config/aerospace/open_notes.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Check if Wezterm is installed
-if ! command -v wezterm &> /dev/null; then
-    echo "Wezterm is not installed or not in the PATH."
-    exit 1
+if ! command -v wezterm &>/dev/null; then
+  echo "Wezterm is not installed or not in the PATH."
+  exit 1
 fi
 
 # Check if Wezterm is already running
-wezterm start --cwd ~/personal/notes --class wezterm_notes -- notes zsh -c "nvim" > /dev/null 2>&1 &
+wezterm start --cwd ~/personal/notes --class wezterm_notes -- notes zsh -c "nvim" >/dev/null 2>&1 &
 
 # Move to the notes workspace
 aerospace workspace 5
