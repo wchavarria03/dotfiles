ca2d7dc248faae5c6da90d6fa81b780a267f9e20
Walter Chavarria <wchavarria03@gmail.com>
2025-03-27 21:27:58 -0600

    feat: Better brew installed packages check

--- setup/install_brew_packages.sh
+++ setup/install_brew_packages.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# Function to check if a Brew package is installed
+is_brew_package_installed() {
+    local package_name="$1"
+    brew list "$package_name" &>/dev/null
+    return $?
+}
+
 # Function to install or update Brew packages
 install_brew_packages() {
     # Borders in conjuntion with AeroSpace
     echo "${COLOR_GREEN}-- Installing tap formulae${COLOR_RESET}"
     brew tap FelixKratz/formulae
+
 
     local packages=(
         asdf
@@ -17,7 +25,6 @@ install_brew_packages() {
         glow # markdown previewer
         neovim
         npm
-        python3
         ripgrep
         starship
         nvim
@@ -33,11 +40,17 @@ install_brew_packages() {
         sequel-ace
         slack
         1password
-        1password/tap/1password-cli
+        1password-cli
         obsidian
         raycast
-        wezterm
+        kitty
+        ghostty
 
+        # CLI apps
+        atac
+        dblab
+
+        ## Desktop Manager
         aeroSpace
         borders # requires formulae tap
 
@@ -49,18 +62,17 @@ install_brew_packages() {
     )
 
     for package in "${packages[@]}"; do
-        if brew info "$package" &>/dev/null; then
-            echo "${COLOR_GREEN}-- $package already installed.${COLOR_RESET}"
-        else
-            echo "${COLOR_GREEN}-- Installing $package...${COLOR_RESET}"
+        if ! is_brew_package_installed "$package"; then
+            echo "Installing $package..."
             brew install "$package"
+        else
+            echo "$package is already installed."
         fi
     done
 
     echo "${COLOR_GREEN}-- Cleaning up Brew...${COLOR_REST}"
     brew cleanup
 }
-
 install_brew_packages_clean_up() {
     echo "${COLOR_GREEN}Install Brew packages clean up - coming feature...${COLOR_RESET}"
 }
