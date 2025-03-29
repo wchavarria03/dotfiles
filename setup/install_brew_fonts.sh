615b8114527322d140626125538ddb98493386d1
Walter Chavarria <wchavarria03@gmail.com>
2025-03-29 15:33:15 -0600

    Removed unused fonts

--- setup/install_brew_fonts.sh
+++ setup/install_brew_fonts.sh
@@ -2,22 +2,20 @@
 
 # Function to install brew fonts
 install_brew_fonts() {
-    local fonts=(
-        font-meslo-lg-nerd-font
-        font-fira-code
-        font-jetbrains-mono
-    )
+  local fonts=(
+    font-jetbrains-mono
+  )
 
-    for font in "${fonts[@]}"; do
-        if brew info "$font" &>/dev/null; then
-            echo "${COLOR_GREEN}-- $font already installed.${COLOR_RESET}"
-        else
-            echo "${COLOR_GREEN}-- Installing $font...${COLOR_RESET}"
-            brew install --cask "$font"
-        fi
-    done
+  for font in "${fonts[@]}"; do
+    if brew info "$font" &>/dev/null; then
+      echo "${COLOR_GREEN}-- $font already installed.${COLOR_RESET}"
+    else
+      echo "${COLOR_GREEN}-- Installing $font...${COLOR_RESET}"
+      brew install --cask "$font"
+    fi
+  done
 }
 
 install_brew_fonts_clean_up() {
-    echo "${COLOR_GREEN}Install Brew fonts clean up - coming feature...${COLOR_RESET}"
+  echo "${COLOR_GREEN}Install Brew fonts clean up - coming feature...${COLOR_RESET}"
 }
