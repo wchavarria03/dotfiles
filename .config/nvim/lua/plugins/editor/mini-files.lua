0370739de7e60aa01cbe25bd453d9fbb579cafb7
Walter Chavarria <wchavarria03@gmail.com>
2025-05-06 14:51:26 -0600

    feat: Configure mini-files and add keymaps

--- .config/nvim/lua/plugins/editor/mini-files.lua
+++ .config/nvim/lua/plugins/editor/mini-files.lua
@@ -6,12 +6,25 @@ return {
     {
       "<leader>e",
       function()
-        require("mini.files").open()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
       desc = "Explorer Mini Files",
     },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open()
+      end,
+      desc = "Explorer Mini Files (Current Directory)",
+    },
   },
-  config = function()
-    require("mini.files").setup({})
+  opts = {
+    options = {
+      parmanent_delete = false,
+      use_as_default_explorer = true,
+    },
+  },
+  config = function(_, opts)
+    require("mini.files").setup(opts)
   end,
 }
