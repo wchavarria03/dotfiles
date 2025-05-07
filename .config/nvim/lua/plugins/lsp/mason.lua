70ed3ecff9245cbfdc6f986da4f22465e79aeedf
Walter Chavarria <wchavarria03@gmail.com>
2025-05-07 16:41:33 -0600

    feat: Indentation + yaml lsp config

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,50 +1,53 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    opts = {
-      ui = {
-        border = "single",
-      },
-    },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          -- Bash
-          "bashls",
-
-          -- BeanCount
-          "beancount",
-
-          -- Docker
-          "dockerls",
-          "docker_compose_language_service",
-
-          -- Go
-          "gopls",
-
-          -- JSON
-          "jsonls",
-
-          -- Lua
-          "lua_ls",
-
-          -- Markdown
-          "marksman",
-
-          -- Ruby
-          -- "ruby_lsp", -- Having issues installing this one
-
-          -- TypeScript / Javascript
-          "ts_ls",
+    {
+        'williamboman/mason.nvim',
+        cmd = 'Mason',
+        build = ':MasonUpdate',
+        opts = {
+            ui = {
+                border = 'single',
+            },
         },
-        automatic_installation = true,
-      })
-    end,
-  },
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = { 'williamboman/mason.nvim' },
+        config = function()
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    -- Bash
+                    'bashls',
+
+                    -- BeanCount
+                    'beancount',
+
+                    -- Docker
+                    'dockerls',
+                    'docker_compose_language_service',
+
+                    -- Go
+                    'gopls',
+
+                    -- JSON
+                    'jsonls',
+
+                    -- Lua
+                    'lua_ls',
+
+                    -- Markdown
+                    'marksman',
+
+                    -- Ruby
+                    -- "ruby_lsp", -- Having issues installing this one
+
+                    -- TypeScript / Javascript
+                    'ts_ls',
+
+                    -- YAML
+                    'yamlls',
+                },
+                automatic_installation = true,
+            }
+        end,
+    },
 }
