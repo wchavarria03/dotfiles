7875a05586fa6d57476ad11338a1d895535b706f
Walter Chavarria <wchavarria03@gmail.com>
2025-06-04 12:34:47 -0600

    feat: Update lazy dependencies versions and linter fixes

--- .config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
@@ -1,86 +1,86 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  event = { "VeryLazy" },
-  init = function(plugin)
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    { "<c-space>", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
-  },
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = {
-      "bash",
-      "beancount",
-      "diff",
-      "css",
-      "csv",
-      "dockerfile",
-
-      -- Git
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-
-      -- Go
-      "go",
-      "gomod",
-      "gosum",
-      "gotmpl",
-      "gowork",
-
-      --JSON
-      "json5",
-
-      -- Python
-      "ninja",
-      "rst",
-
-      "html",
-      "javascript",
-      "jsdoc",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "make",
-      "markdown",
-      "markdown_inline",
-      "nginx",
-      "printf",
-      "python",
-      "query",
-      "regex",
-      "ruby",
-      "scss",
-      "terraform",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
+    'nvim-treesitter/nvim-treesitter',
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ':TSUpdate',
+    event = { 'VeryLazy' },
+    init = function(plugin)
+        require('lazy.core.loader').add_to_rtp(plugin)
+        require 'nvim-treesitter.query_predicates'
+    end,
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    keys = {
+        { '<c-space>', desc = 'Increment Selection' },
+        { '<bs>', desc = 'Decrement Selection', mode = 'x' },
     },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            'bash',
+            'beancount',
+            'diff',
+            'css',
+            'csv',
+            'dockerfile',
+
+            -- Git
+            'git_config',
+            'git_rebase',
+            'gitattributes',
+            'gitcommit',
+            'gitignore',
+
+            -- Go
+            'go',
+            'gomod',
+            'gosum',
+            'gotmpl',
+            'gowork',
+
+            --JSON
+            'json5',
+
+            -- Python
+            'ninja',
+            'rst',
+
+            'html',
+            'javascript',
+            'jsdoc',
+            'jsonc',
+            'lua',
+            'luadoc',
+            'luap',
+            'make',
+            'markdown',
+            'markdown_inline',
+            'nginx',
+            'printf',
+            'python',
+            'query',
+            'regex',
+            'ruby',
+            'scss',
+            'terraform',
+            'toml',
+            'tsx',
+            'typescript',
+            'vim',
+            'vimdoc',
+            'xml',
+            'yaml',
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '<C-space>',
+                node_incremental = '<C-space>',
+                scope_incremental = false,
+                node_decremental = '<bs>',
+            },
+        },
     },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+    config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
+    end,
 }
