0479a6878cf68d30cd5bb7916c518609a53f1375
Walter Chavarria <wchavarria03@gmail.com>
2025-06-04 14:41:18 -0600

    fix: Linter errors in the nvim-lint

--- .config/nvim/lua/plugins/linting/nvim-lint.lua
+++ .config/nvim/lua/plugins/linting/nvim-lint.lua
@@ -1,30 +1,31 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = "VeryLazy",
-  dependecies = {
-    "rshkarin/mason-nvim-lint",
-  },
-  opts = {
-    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-    linters_by_ft = {
-      bash = { "shellcheck" },
-      beancount = { "bean_check" },
-      dockerfile = { "hadolint" },
-      go = { "golangcilint" },
-      javascript = { "eslint_d" },
-      json = { "biome" },
-      lua = { "luacheck" },
-      python = { "flake8" },
-      typescript = { "eslint_d" },
-      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-      ["_"] = { "cspell", "codespell" },
+    'mfussenegger/nvim-lint',
+    event = 'VeryLazy',
+    dependecies = {
+        'rshkarin/mason-nvim-lint',
     },
-  },
-  config = function()
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  end,
+    opts = {
+        events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
+        linters_by_ft = {
+            bash = { 'shellcheck' },
+            beancount = { 'bean_check' },
+            dockerfile = { 'hadolint' },
+            go = { 'golangcilint' },
+            javascript = { 'eslint_d' },
+            json = { 'biome' },
+            lua = { 'luacheck' },
+            python = { 'flake8' },
+            typescript = { 'eslint_d' },
+            solidity = { 'solhint' },
+            -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+            ['_'] = { 'cspell', 'codespell' },
+        },
+    },
+    config = function()
+        vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+            callback = function()
+                require('lint').try_lint()
+            end,
+        })
+    end,
 }
