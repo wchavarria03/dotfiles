093acac28f16856c8c1ce5138ae908df05df37f8
Walter Chavarria <wchavarria03@gmail.com>
2025-09-15 17:48:58 -0600

    feat(wezterm): Updated editor config

--- .config/wezterm/editor.lua
+++ .config/wezterm/editor.lua
@@ -6,16 +6,14 @@ function module.apply_to_config(config)
 	config.font = wezterm.font("JetBrains Mono NL", { weight = "Regular" })
 	config.font_size = 14
 
-	config.window_padding = { left = 10, right = 10, top = 10, bottom = 10 }
-	
-	-- Window styling only (tabs handled in tabbar.lua)
-	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-	
+	config.window_padding = { left = 20, right = 20, top = 20, bottom = 10 }
+
+	config.window_decorations = "RESIZE"
+
 	config.quit_when_all_windows_are_closed = true
-	
+
 	config.window_background_opacity = 0.9
 	config.macos_window_background_blur = 25
-	config.text_background_opacity = 1.0
 end
 
 return module
