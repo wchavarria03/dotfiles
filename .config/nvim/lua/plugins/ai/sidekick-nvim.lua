d8eb31c97694dfe12a0eba6b9f638d0a9ce60969
Walter Chavarria <wchavarria03@gmail.com>
2025-10-01 18:31:31 -0600

    feat(ai): Sidekick config imroved

--- .config/nvim/lua/plugins/ai/sidekick-nvim.lua
+++ .config/nvim/lua/plugins/ai/sidekick-nvim.lua
@@ -25,20 +25,23 @@ return {
             '<tab>',
             function()
                 -- if there is a next edit, jump to it, otherwise apply it if any
-                if not require('sidekick').nes_jump_or_apply() then
-                    return '<Tab>' -- fallback to normal tab
+                if require('sidekick').nes_jump_or_apply() then
+                    return -- jumped or applied
                 end
+
+                -- if you are using Neovim's native inline completions
+                if vim.lsp.inline_completion.get() then
+                    return
+                end
+
+                -- any other things (like snippets) you want to do on <tab> go here.
+
+                -- fall back to normal tab
+                return '<tab>'
             end,
+            mode = { 'i', 'n' },
             expr = true,
             desc = 'Goto/Apply Next Edit Suggestion',
-        },
-        {
-            '<c-.>',
-            function()
-                require('sidekick.cli').focus()
-            end,
-            desc = 'Sidekick Switch Focus',
-            mode = { 'n', 'v' },
         },
         {
             '<leader>aa',
@@ -57,9 +60,17 @@ return {
             mode = { 'n', 'v' },
         },
         {
+            '<leader>ag',
+            function()
+                require('sidekick.cli').toggle { name = 'gemini', focus = true }
+            end,
+            desc = 'Sidekick Gemini Toggle',
+            mode = { 'n', 'v' },
+        },
+        {
             '<leader>ap',
             function()
-                require('sidekick.cli').select_prompt()
+                require('sidekick.cli').prompt()
             end,
             desc = 'Sidekick Ask Prompt',
             mode = { 'n', 'v' },
--- .config/nvim/lua/plugins/coding/blink.lua
+++ .config/nvim/lua/plugins/coding/blink.lua
@@ -43,6 +43,16 @@ return {
         },
         keymap = {
             preset = 'default',
+            ['<Tab>'] = {
+                'snippet_forward',
+                function() -- sidekick next edit suggestion
+                    return require('sidekick').nes_jump_or_apply()
+                end,
+                function() -- if you are using Neovim's native inline completions
+                    return vim.lsp.inline_completion.get()
+                end,
+                'fallback',
+            },
         },
     },
     config = function(_, opts)
