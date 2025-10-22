86f998588c48947736853d5d22c37c14775cbd64
Walter Chavarria <wchavarria03@gmail.com>
2025-10-22 04:22:54 -0600

    feat(snacks): LSP progress indicator

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -183,13 +183,13 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
         vim.opt_local.softtabstop = 2
         vim.opt_local.shiftwidth = 2
         vim.opt_local.expandtab = true
-        
+
         -- Note-specific text wrapping
         vim.opt_local.wrap = true
         vim.opt_local.linebreak = true
         vim.opt_local.breakindent = true
         vim.opt_local.showbreak = '↪ '
-        
+
         -- Note-specific spell checking
         vim.opt_local.spell = true
         vim.opt_local.conceallevel = 2
@@ -205,4 +205,20 @@ vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
             vim.cmd 'silent! write'
         end
     end,
-}) 
+})
+
+vim.api.nvim_create_autocmd('LspProgress', {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        vim.notify(vim.lsp.status(), 'info', {
+            id = 'lsp_progress',
+            title = 'LSP Progress',
+            opts = function(notif)
+                notif.icon = ev.data.params.value.kind == 'end' and ' '
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
+
