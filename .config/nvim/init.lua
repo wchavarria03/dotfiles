c6eb6fd75fe9e53f37b8f4d19e1c2e3f68acb5af
Walter Chavarria <wchavarria03@gmail.com>
2025-10-26 16:32:10 -0600

    chore(plugins): Reorganize plugins order

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,34 +10,42 @@ require 'core.autocommands'
 require 'plugins.colorscheme'
 require 'plugins.snacks'
 
+-- Core Utils (must load early, used by other plugins)
+require 'plugins.utils.plenary'
+require 'plugins.utils.nvim-web-devicons'
+
+-- Treesitter (must load before editor plugins and languages)
+require 'plugins.treesitter.treesitter'
+
 -- Editor
 require 'plugins.editor.setup'
 
--- LSP
+-- Coding (blink.cmp must load before LSP for capabilities)
+require 'plugins.coding.setup'
+
+-- LSP (depends on blink.cmp capabilities)
 require 'plugins.lsp.setup'
 
 -- AI
 require 'plugins.ai.setup'
 
--- Coding
-require 'plugins.coding.setup'
-
 -- Linting
 require 'plugins.linting.setup'
 
--- Formating
+-- Formatting
 require 'plugins.formatting.conform-nvim'
 
 -- UI
 require 'plugins.ui.lualine'
 
--- Treesitter
-require 'plugins.treesitter.treesitter'
-
--- Utils
-require 'plugins.utils.setup'
+-- Additional Utils (non-core utilities)
+require 'plugins.utils.kulala'
+require 'plugins.utils.cloak'
+require 'plugins.utils.smart-splits'
+require 'plugins.utils.hardtime'
+require 'plugins.utils.precognition'
+require 'plugins.utils.undo-tree'
 pcall(require, 'undotree')
 
--- Languages
+-- Languages (render-markdown depends on treesitter)
 require 'plugins.languages.setup'
-
